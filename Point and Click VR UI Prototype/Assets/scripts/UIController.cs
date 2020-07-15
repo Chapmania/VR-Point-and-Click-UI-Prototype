9b9297feb9b11e8039c5e9f26e4ec5ea31ad93a9
fredta81 <freddyta99@gmail.com>
2020-07-15 15:08:03 +0100

    Full SUDS input

--- Point and Click VR UI Prototype/Assets/scripts/UIController.cs
+++ Point and Click VR UI Prototype/Assets/scripts/UIController.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour {
 
+    private int currentLevel = 0; //Would normally be in master script / scene controller
+    private int sudsValue = 0;
+
+    public Text SUDSRecord;
+    public Text levelNumber;
     public Text output;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentLevel = 0;
+        updateLevelText();
     }
 
     // Update is called once per frame
@@ -30,5 +36,28 @@ public class UIController : MonoBehaviour {
 
     public void SliderChange(Slider slider) {
         output.text = slider.value.ToString();
+        sudsValue = (int)slider.value;
     }
+
+    public void NextLevelPress() {
+        updateSUDS();
+        currentLevel += 1;
+        updateLevelText();
+    }
+
+    public void PrevLevelPress() {
+        //Don't want to submit score if we don't finish the level
+        currentLevel -= 1;
+        updateLevelText();
+    }
+
+    private void updateSUDS() {
+        SUDSRecord.text += "L" + currentLevel + ": " + sudsValue + ", ";
+    }
+
+    private void updateLevelText() {
+        levelNumber.text = "LEVEL: " + currentLevel;
+    }
+
+
 }
